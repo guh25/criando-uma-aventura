1e36aacc58526dd80597069fdcdfd4171487f25e
guh25 <gustavo.da.silva26@escola.pr.gov.br>
2025-10-24 11:26:04 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avanca = document.querySelector('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(buton => {
-    buton.addEventListener('click', function(){
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     })
-});
+})
